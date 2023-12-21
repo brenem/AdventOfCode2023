676b421ab9af606ccddb87b641a88dcb0709aec1
Brett Nemec <brett.nemec@ayahealthcare.com>
2023-12-20 21:30:10 -0500

    day 13

--- AdventOfCode2023.Tests/Day13Tests.cs
+++ AdventOfCode2023.Tests/Day13Tests.cs
@@ -4,19 +4,66 @@ namespace AdventOfCode2023.Tests;
 
 public class Day13Tests
 {
-    private string[] _testInput = new[]
-    {
-        ""
-    };
-
     [Fact]
     public void Part1Test()
     {
         var day = new Day13();
 
-        var result = day.Part1(_testInput);
+        var testInput = new[]
+        {
+            "#.##..##.",
+            "..#.##.#.",
+            "##......#",
+            "##......#",
+            "..#.##.#.",
+            "..##..##.",
+            "#.#.##.#.",
+            "",
+            "#...##..#",
+            "#....#..#",
+            "..##..###",
+            "#####.##.",
+            "#####.##.",
+            "..##..###",
+            "#....#..#"
+        };
 
-        result.Should().Be(6440);
+        var result = day.Part1(testInput);
+
+        result.Should().Be(405);
+    }
+
+    [Fact]
+    public void Part1Test2()
+    {
+        var day = new Day13();
+
+        var input = new[]
+        {
+            "###.##.##",
+            "##.####.#",
+            "##.#..#.#",
+            "####..###",
+            "....##...",
+            "##.#..#.#",
+            "...#..#..",
+            "##..###.#",
+            "##......#",
+            "##......#",
+            "..#.##.#.",
+            "...#..#..",
+            "##.####.#",
+            "....##...",
+            "...####..",
+            "....##...",
+            "##.####.#"
+        };
+
+        var lines = input.Select((x, i) => (i, x)).ToList();
+
+        var result = day.FindLinesOfReflection(lines);
+
+        result.Should().Be(10);
     }
 
     [Fact]
@@ -24,8 +71,27 @@ public class Day13Tests
     {
         var day = new Day13();
 
-        var result = day.Part2(_testInput);
+        var testInput = new[]
+        {
+            "#.##..##.",
+            "..#.##.#.",
+            "##......#",
+            "##......#",
+            "..#.##.#.",
+            "..##..##.",
+            "#.#.##.#.",
+            "",
+            "#...##..#",
+            "#....#..#",
+            "..##..###",
+            "#####.##.",
+            "#####.##.",
+            "..##..###",
+            "#....#..#"
+        };
 
-        result.Should().Be(5905);
+        var result = day.Part2(testInput);
+
+        result.Should().Be(400);
     }
 }
--- AdventOfCode2023/Days/Day13.cs
+++ AdventOfCode2023/Days/Day13.cs
@@ -3,13 +3,217 @@
 
 public class Day13
 {
-    public int Part1(string[] input)
+    public long Part1(string[] input)
     {
-        throw new NotImplementedException();
+        long total = 0;
+
+        var patterns = ParseInput(input).ToList();
+
+        bool CheckHorizontal(List<string> pattern, int row)
+        {
+            for (int i = row - 1, j = row; i >= 0 && j < pattern.Count; i--, j++)
+            {
+                if (pattern[i] != pattern[j])
+                    return false;
+            }
+
+            return true;
+        }
+
+        foreach (var pattern in patterns)
+        {
+            var horizontalFound = false;
+
+            for (var i = 1; i < pattern.Count; i++)
+            {
+                if (CheckHorizontal(pattern, i))
+                {
+                    total += 100 * i;
+                    horizontalFound = true;
+                    break;
+                }
+            }
+
+            if (horizontalFound)
+                continue;
+
+            var pivoted = Pivot(pattern);
+            for (var i = 1; i <= pivoted.Count; i++)
+            {
+                if (CheckHorizontal(pivoted, i))
+                {
+                    total += i;
+                    break;
+                }
+            }
+        }
+
+        return total;
     }
 
-    public int Part2(string[] input)
+    public long Part2(string[] input)
     {
-        throw new NotImplementedException();
+        long total = 0;
+
+        var patterns = ParseInput(input).ToList();
+
+        bool CheckHorizontal(List<string> pattern, int row)
+        {
+            int? smudgeRow = null, smudgeCol = null;
+
+            for (int i = row - 1, j = row; i >= 0 && j < pattern.Count; i--, j++)
+            {
+                string pi = pattern[i], pj = pattern[j];
+                for (int k = 0; k < pi.Length; k++)
+                {
+                    if (pi[k] != pj[k])
+                    {
+                        if (smudgeRow is not null)
+                            return false;
+                        smudgeRow = i;
+                    }
+                }
+            }
+
+            return smudgeRow != null;
+        }
+
+        foreach (var pattern in patterns)
+        {
+            var horizontalFound = false;
+
+            for (var i = 1; i < pattern.Count; i++)
+            {
+                if (CheckHorizontal(pattern, i))
+                {
+                    total += 100 * i;
+                    horizontalFound = true;
+                    break;
+                }
+            }
+
+            if (horizontalFound)
+                continue;
+
+            var pivoted = Pivot(pattern);
+            for (var i = 1; i <= pivoted.Count; i++)
+            {
+                if (CheckHorizontal(pivoted, i))
+                {
+                    total += i;
+                    break;
+                }
+            }
+        }
+
+        return total;
+    }
+
+    IEnumerable<List<string>> ParseInput(string[] input)
+    {
+        var lastEmptyIndex = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i] == string.Empty)
+            {
+                yield return [.. input[lastEmptyIndex..i]];
+                lastEmptyIndex = i + 1;
+            }
+
+            if ((i + 1) == input.Length)
+                yield return [.. input[lastEmptyIndex..(i + 1)]];
+        }
+    }
+
+    public long FindLinesOfReflection(List<(int Idx, string Line)> input)
+    {
+        var horizontalLines = 0;
+        var verticalLines = 0;
+
+        var horizontalGroups = input.GroupBy(x => x.Line);
+        var horizontalMatches = horizontalGroups.Where(x => x.Count() >= 2).ToList();
+        var hasHorizontalReflection = true;
+        int? horizontalTopIndex = null;
+
+        for (var i = 1; i < input.Count; i += 2)
+        {
+            var matches = horizontalMatches.Where(x => Math.Abs(x.ElementAt(0).Idx - x.ElementAt(1).Idx) == i);
+            if (matches.Any())
+            {
+                if (i == 1)
+                {
+                    horizontalTopIndex = matches.SelectMany(x => x.Select(g => g)).OrderBy(x => x.Idx).First().Idx;
+                }
+                else
+                {
+                    if (matches.Any(x => x.ElementAt(0).Idx == 0 || x.ElementAt(1).Idx == input.Count - 1))
+                        break;
+                }
+            }
+            else
+            {
+                hasHorizontalReflection = false;
+                break;
+            }
+        }
+
+        if (hasHorizontalReflection && horizontalTopIndex != null)
+        {
+            return (horizontalTopIndex.Value + 1) * 100;
+        }
+
+        var pivoted = PivotIndexed(input.Select(x => x.Line).ToList());
+        var verticalGroups = pivoted.GroupBy(x => x.Line);
+        var verticalMatches = verticalGroups.Where(x => x.Count() >= 2).ToList();
+        var hasVerticalReflection = true;
+        int? verticalTopIndex = null;
+
+        for (var i = 1; i < input.Count; i += 2)
+        {
+            var matches = verticalMatches.Where(x => Math.Abs(x.ElementAt(0).Idx - x.ElementAt(1).Idx) == i);
+            if (matches.Any())
+            {
+                if (i == 1)
+                {
+                    verticalTopIndex = matches.SelectMany(x => x.Select(g => g)).OrderBy(x => x.Idx).First().Idx;
+                }
+                else
+                {
+                    if (matches.Any(x => x.ElementAt(0).Idx == 0 || x.ElementAt(1).Idx == input.Count - 1))
+                        break;
+                }
+            }
+            else
+            {
+                hasVerticalReflection = false;
+                break;
+            }
+        }
+
+        if (hasVerticalReflection && verticalTopIndex != null)
+        {
+            return verticalTopIndex.Value + 1;
+        }
+
+        return 0;
+    }
+
+    List<(int Idx, string Line)> PivotIndexed(List<string> input)
+    {
+        var length = input[0].Length;
+        var retVal = new List<(int Idx, string Line)>();
+
+        for (int i = 0; i < length; i++)
+        {
+            retVal.Add((i, new string(input.Select(x => x[i]).ToArray())));
+        }
+
+        return retVal;
+    }
+
+    List<string> Pivot(List<string> input)
+    {
+        return PivotIndexed(input).Select(x => x.Line).ToList();
     }
 }
