370beb789f955f1c442270d431a2674e704b6aaa
Brett Nemec <brett.nemec@ayahealthcare.com>
2023-12-21 15:29:25 -0500

    day 15

--- AdventOfCode2023.Tests/Day15Tests.cs
+++ AdventOfCode2023.Tests/Day15Tests.cs
@@ -4,19 +4,28 @@ namespace AdventOfCode2023.Tests;
 
 public class Day15Tests
 {
-    private string[] _testInput = new[]
-    {
-        ""
-    };
-
     [Fact]
     public void Part1Test()
     {
         var day = new Day15();
 
-        var result = day.Part1(_testInput);
+        var testInput = "HASH";
 
-        result.Should().Be(6440);
+        var result = day.CalculateHashSum(testInput);
+
+        result.Should().Be(52);
+    }
+
+    [Fact]
+    public void Part1Test2()
+    {
+        var day = new Day15();
+
+        var testInput = new[] { "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7" };
+
+        var result = day.Part1(testInput);
+
+        result.Should().Be(1320);
     }
 
     [Fact]
@@ -24,8 +33,10 @@ public class Day15Tests
     {
         var day = new Day15();
 
-        var result = day.Part2(_testInput);
+        var testInput = new[] { "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7" };
 
-        result.Should().Be(5905);
+        var result = day.Part2(testInput);
+
+        result.Should().Be(145);
     }
 }
--- AdventOfCode2023/Days/Day15.cs
+++ AdventOfCode2023/Days/Day15.cs
@@ -5,11 +5,98 @@ public class Day15
 {
     public int Part1(string[] input)
     {
-        throw new NotImplementedException();
+        var steps = input[0].Split(',');
+        var totalHashSum = steps.Select(CalculateHashSum).Sum();
+        return totalHashSum;
     }
 
     public int Part2(string[] input)
     {
-        throw new NotImplementedException();
+        var steps = input[0].Split(',');
+        var boxes = new Dictionary<int, Queue<(string Label, int FocalLength)>>();
+
+        foreach (var step in steps)
+        {
+            var stepParts = step.Split('=', '-');
+            var label = stepParts[0];
+            var boxId = CalculateHashSum(label);
+
+            Queue<(string Label, int FocalLength)> lensQueue;
+            if (boxes.TryGetValue(boxId, out Queue<(string Label, int FocalLength)> value))
+            {
+                lensQueue = value;
+            }
+            else
+            {
+                lensQueue = new Queue<(string Label, int FocalLength)>();
+                boxes.Add(boxId, lensQueue);
+            }
+
+            if (step.Contains('-'))
+            {
+                var lenses = lensQueue.ToList();
+                var lensToRemove = lenses.Find(x => x.Label == label);
+                if (lensToRemove != default)
+                {
+                    lenses.Remove(lensToRemove);
+
+                    lensQueue.Clear();
+                    lenses.ForEach(x => lensQueue.Enqueue(x));
+                }
+            }
+            else
+            {
+                var focalLength = int.Parse(stepParts[1]);
+                var lens = (label, focalLength);
+
+                var lenses = lensQueue.ToList();
+                var existingLensIndex = lenses.FindIndex(x => x.Label == label);
+                if (existingLensIndex != -1)
+                {
+                    lenses[existingLensIndex] = lens;
+
+                    lensQueue.Clear();
+                    lenses.ForEach(x => lensQueue.Enqueue(x));
+                }
+                else
+                {
+                    lensQueue.Enqueue(lens);
+                }
+            }
+        }
+
+        var totalFocusingPower = 0;
+        var boxesWithLenses = boxes.Where(x => x.Value.Count > 0);
+
+        foreach (var box in boxesWithLenses)
+        {
+            var boxId = box.Key;
+            var lenses = box.Value;
+
+            var lensSlot = 1;
+            while (lenses.TryDequeue(out (string Label, int FocalLength) lens))
+            {
+                var (_, focalLength) = lens;
+                totalFocusingPower += (1 + boxId) * lensSlot * focalLength;
+
+                lensSlot++;
+            }
+        }
+
+        return totalFocusingPower;
+    }
+
+    public int CalculateHashSum(string input)
+    {
+        var hashSum = 0;
+
+        foreach (var ch in input)
+        {
+            hashSum += ch;
+            hashSum *= 17;
+            hashSum %= 256;
+        }
+
+        return hashSum;
     }
 }
